8fcade15db5738147468f39512fb5d5968c446b8
pierobramanti <piero.bramanti94@gmail.com>
2024-07-31 23:20:19 +0200

    [UPD] inserted v-for to create 20 cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,12 +9,31 @@ export default {
 </script>
 <template>
     <main class="py-50">
-        <Cards />
+        <div class="card-container mx-auto rounded-20">
+            <div class="bg-dark rounded-t-20">
+                <p class="ms-30 py-10">Found cards</p>
+            </div>
+            <div class="d-flex flex-wrap flex-justify-center p-20">
+                <Cards />
+            </div>
+        </div>
     </main>
 </template>
 <style lang="scss" scoped>
+@use "../components/styles/partials/vars.scss" as *;
+
 main {
     height: 500px;
     background-color: $background_color;
+    .card-container {
+        background-color: white;
+        max-width: 900px;
+        .bg-dark {
+            background-color: $bg_dark;
+            p {
+                color: white;
+            }
+        }
+    }
 }
 </style>
--- src/components/partials/Cards.vue
+++ src/components/partials/Cards.vue
@@ -1,17 +1,30 @@
 <script>
+
+import { store } from '../../store';
 export default {
-    
+    data() {
+        return {
+            store
+        }
+    }
 }
 </script>
 <template>
     <div class="mx-auto rounded-20">
-        <h1>ciao</h1>
+        <div class="card col-16 m-10x5" v-for="card in store.cardList" :key="card.id">
+            <div class="square">
+                <img class="mxw-100" v-for="(img, index) in card.card_images" :key="`img-${index}`" :src="img.image_url" alt="">
+            </div>
+            <p>{{card.name}}</p>
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    div {
-        background-color: white;
-        width: 800px;
-        height: 200px;
+.card {
+    border: 1px solid black;
+    @media screen and (max-width: 800px){
+            width: calc(100% / 3);
     }
+    
+}
 </style>
