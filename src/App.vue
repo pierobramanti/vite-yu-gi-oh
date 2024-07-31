808c69ed01c38405e5cc851e69f2c906b5cb5c72
pierobramanti <piero.bramanti94@gmail.com>
2024-07-31 16:24:25 +0200

    [UPD] request

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script>
+import axios from 'axios';
 import { store } from './store';
 export default {
   data() {
     return {
       store
+    }
+  },
+  created() {
+    this.getCards()
+  },
+  methods: {
+    getCards(){
+      axios.get(store.url).then((res) => {
+        store.cardList = res.data.data
+      })
     }
   }
 }
--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { reactive } from "vue"
 export const store = reactive({
-
+    cardList: [],
+    url: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0"
 })
