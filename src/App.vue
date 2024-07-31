1f4411d7d64ec68bd3859b2974e8707c31f68d89
pierobramanti <piero.bramanti94@gmail.com>
2024-07-31 16:43:18 +0200

    [UPD] created main.vue and header.vue and imported in app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script>
 import axios from 'axios';
 import { store } from './store';
+
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+
+
 export default {
+  components: {
+    Header,
+    Main
+  },
   data() {
     return {
       store
@@ -21,7 +30,8 @@ export default {
 </script>
 
 <template>
-
+  <Header />
+  <Main />
 </template>
 
 <style scoped>
